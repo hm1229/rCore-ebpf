36673456c28ca3673ea57576a7e6293b2df863a3
hm <qq171928059@163.com>
2021-12-15 17:53:35 -0800

    enable new probe mode && add instruction decode

--- kernel/src/kprobes/mod.rs
+++ kernel/src/kprobes/mod.rs
@@ -1,11 +1,15 @@
 mod kprobes;
+mod riscv_insn_decode;
+
+use riscv_insn_decode::{insn_decode, InsnStatus};
 use alloc::sync::Arc;
 pub use kprobes::kprobes_trap_handler;
 use spin::Mutex;
 use trapframe::TrapFrame;
+pub use kprobes::ProbeType;
 
-pub fn kprobe_register(addr: usize, handler: Arc<Mutex<dyn FnMut(&mut TrapFrame) + Send>>, post_handler: Option<Arc<Mutex<dyn FnMut(&mut TrapFrame) + Send>>>) -> isize {
-    kprobes::KPROBES.register_kprobe(addr, handler, post_handler)
+pub fn kprobe_register(addr: usize, handler: Arc<Mutex<dyn FnMut(&mut TrapFrame) + Send>>, post_handler: Option<Arc<Mutex<dyn FnMut(&mut TrapFrame) + Send>>>, probe_type: ProbeType) -> isize {
+    kprobes::KPROBES.register_kprobe(addr, handler, post_handler, probe_type)
 }
 
 pub fn kprobe_unregister(addr: usize) -> isize {
